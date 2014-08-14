ac24d7a6a4cf8aa800ae27631dff3457bd97d0f2
Hugo Landau <hlandau@devever.net>
2014-08-14 21:26:26 +0100

    supervisor test

--- supervise/supervise.go
+++ supervise/supervise.go
@@ -1,5 +1,9 @@
 // WORK IN PROGRESS
 package supervise
+import "github.com/hlandau/degoutils/log"
+import "github.com/hlandau/degoutils/net"
+import "fmt"
+import "time"
 
 const (
   MET_NormalExit  = 0
@@ -10,25 +14,27 @@ const (
 type MonitorEvent struct {
   Type int
   PanicValue interface{}
+  ReturnError error
 }
 
-func Monitor(f func()) chan MonitorEvent {
+func Monitor(f func() error) <-chan MonitorEvent {
   ch := make(chan MonitorEvent, 10)
   go func() {
     var normalExit bool = false
+    var err error
     defer func() {
       r := recover()
       if r != nil {
         // recover ...
-        ch <- MonitorEvent{MET_PanicExit, r}
+        ch <- MonitorEvent{MET_PanicExit, r, nil}
       } else if normalExit {
-        ch <- MonitorEvent{MET_NormalExit, nil}
+        ch <- MonitorEvent{MET_NormalExit, nil, err}
       } else {
-        ch <- MonitorEvent{MET_RuntimeExit, nil}
+        ch <- MonitorEvent{MET_RuntimeExit, nil, nil}
       }
     }()
 
-    f()
+    err = f()
 
     normalExit = true
   }()
@@ -36,6 +42,20 @@ func Monitor(f func()) chan MonitorEvent {
 }
 
 ////////////////////
+
+type Supervisor interface {
+  EventChan() <-chan SupervisionEvent
+  Stop()
+}
+
+const (
+  SET_NormalExit = 1
+  SET_Stopped    = 2
+)
+
+type SupervisionEvent struct {
+  Type int
+}
 
 const (
   SCT_StopSupervising = 1
@@ -45,32 +65,50 @@ type SupervisionCommand struct {
   Type int
 }
 
-func Supervise(f func()) {
-  cch := make(chan SupervisionCommand)
+type supervisor struct {
+  cch  chan SupervisionCommand
+  evch chan SupervisionEvent
+  retryConfig net.RetryConfig
+}
+
+func (s *supervisor) Stop() {
+  s.cch <- SupervisionCommand{SCT_StopSupervising}
+}
+
+func (s *supervisor) EventChan() <-chan SupervisionEvent {
+  return s.evch
+}
+
+func Supervise(f func() error) Supervisor {
+  sup := &supervisor {
+    cch:  make(chan SupervisionCommand),
+    evch: make(chan SupervisionEvent,10),
+  }
+
   go func() {
     ch := Monitor(f)
     for {
       select {
         case e  := <-ch:
-          if e.Type != MET_NormalExit {
+          if e.Type != MET_NormalExit || e.ReturnError != nil {
+            delay := time.Duration(sup.retryConfig.GetStepDelay())*time.Millisecond
+            log.Info(fmt.Sprintf("supervised goroutine exited, restarting in %+v: %+v",delay,e))
+            time.Sleep(delay)
             ch = Monitor(f)
+          } else {
+            sup.evch <- SupervisionEvent{SET_NormalExit}
           }
 
-        case ce := <-cch:
+        case ce := <-sup.cch:
           switch ce.Type {
             case SCT_StopSupervising:
+              close(sup.cch)
+              sup.evch <- SupervisionEvent{SET_Stopped}
               return
           }
       }
     }
   }()
-}
 
-func unused(x interface{}) {}
-
-func main() {
-  ch := Monitor(func() {
-    // ...
-  })
-  unused(ch)
+  return sup
 }
